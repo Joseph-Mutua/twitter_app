9ae1a1479be42a9a851292194605a4b3ed1a8b39
Joseph-Mutua <mutuaj793@gmail.com>
2020-04-07 22:28:49 +0300

    Reset edit action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -18,4 +18,19 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # Confirms a valid user.
+  def valid_user
+    unless (@user && @user.activated? &&
+            @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
 end
+
