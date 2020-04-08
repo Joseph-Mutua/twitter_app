85833f0071a445e77b36abd5749df8b96d381acd
Joseph-Mutua <mutuaj793@gmail.com>
2020-04-08 23:12:22 +0300

    User/Microposts associations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,4 @@
-class Micropost < ActiveRecord::Base
+class Micropost < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
