ef60bcb2a969c226f90f3dc6f40a36faa71e2a10
Joseph Mutua <mutuaj793@gmail.com>
2020-05-22 20:52:36 +0000

    Add the show_follow view used to render following and followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   
-    def following
+  def following
     @title = "Following"
     @user  = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
