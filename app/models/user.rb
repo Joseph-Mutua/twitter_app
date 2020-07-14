efb8e274c10e1e24fcb2fcbaeca1341bc9de7ea0
Joseph Mutua <mutuaj793@gmail.com>
2020-07-14 20:16:54 +0000

    Initial working status feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -121,4 +121,20 @@ class User < ApplicationRecord
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+  
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  # Returns a user's status feed.
+  def feed
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+  
 end
